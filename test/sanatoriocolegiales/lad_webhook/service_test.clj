c45ea5dd64818bee08139a62e8fb2aa5fcbb6446
José Javier Blanco Rivero <javierweiss@gmail.com>
2024-07-31 09:47:37 -0300

    dummy test for testcontainer connection

--- test/sanatoriocolegiales/lad_webhook/service_test.clj
+++ test/sanatoriocolegiales/lad_webhook/service_test.clj
@@ -16,7 +16,7 @@
                                                                             :exposed-ports [5432]}))
                                                          :post-start (fn arrancar-contenedor
                                                                        [{::ds/keys [instance]}]
-                                                                       (tc/map-classpath-resource instance {:resource-path "init.sql"
+                                                                       (tc/map-classpath-resource! instance {:resource-path "init.sql"
                                                                                                             :container-path "/docker-entrypoint-initdb.d/init.sql"
                                                                                                             :mode :read-only})
                                                                        (tc/start! instance))
@@ -34,13 +34,11 @@
                                                                 (.close instance))
                                                         :config {:datasource (ds/ref [:container])}}}}))
 
-(deftest service-test
-  (testing "TODO: Start with a failing test, make it pass, then refactor"
-(let [conn @(ds/instance ds/*system* [:testcontainer :conexion])]
-    ;; TODO: fix greet function to pass test
-  (is (= "sanatoriocolegiales lad-webhook service developed by the secret engineering team"
-         (lad-webhook/greet))))
+(deftest dummy-connection-test
+  (testing ""
+(with-open [conn @(ds/instance ds/*system* [:testcontainer :conexion])] 
+  (is (true? (instance? java.sql.Connection conn))))))
 
-    ;; TODO: fix test by calling greet with {:team-name "Practicalli Engineering"}
-    (is (= (lad-webhook/greet "Practicalli Engineering")
-           "sanatoriocolegiales lad-webhook service developed by the Practicalli Engineering team"))))
+(comment
+  
+  )
