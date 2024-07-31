889c3cee74ab35dd31da8a72f4d139ca9f3274d0
José Javier Blanco Rivero <javierweiss@gmail.com>
2024-07-31 13:14:18 -0300

    Maquetar proceso de validacion

--- src/sanatoriocolegiales/lad_webhook/seguridad/validacion.clj
+++ src/sanatoriocolegiales/lad_webhook/seguridad/validacion.clj
@@ -1,6 +1,27 @@
 (ns sanatoriocolegiales.lad-webhook.seguridad.validacion
-  (:require [buddy.sign.jwt :as jwt]))
+  (:require [buddy.sign.jwt :as jwt]
+            [ring.util.response :refer [status]]
+            [sanatoriocolegiales.lad-webhook.sql.ejecucion :refer [ejecuta!]]
+            [sanatoriocolegiales.lad-webhook.sql.enunciados :refer [selecciona-guardia]]))
 
-(defn validar
+(defn extraer-fecha-y-hora
+  [date-str]
+  )
+
+(defn valida-request
   [req]
-  (constantly true))
+  (if req
+    req
+    (status "Solicitud no autorizada" 401)))
+
+(defn valida-paciente
+  [conn req]
+  (let [{{:keys [datetime event_object]} :body} req
+        {{:keys [uid]} :patient_external_id} event_object
+        [fecha hora] (extraer-fecha-y-hora datetime)
+        paciente (ejecuta! conn (selecciona-guardia uid fecha hora))]
+    (if (> (count paciente) 0)
+      (assoc req :paciente paciente)
+      (throw (ex-info "Paciente no encontrado" {:fecha fecha
+                                                :hora hora
+                                                :hc uid})))))
