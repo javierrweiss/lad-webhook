64d12e8596275aed24962ff7e71c7b6f5c3cce0d
José Javier Blanco Rivero <javierweiss@gmail.com>
2024-08-01 13:23:37 -0300

    Mover funcion a namespace utils

--- src/sanatoriocolegiales/lad_webhook/seguridad/validacion.clj
+++ src/sanatoriocolegiales/lad_webhook/seguridad/validacion.clj
@@ -2,26 +2,25 @@
   (:require [buddy.sign.jwt :as jwt]
             [ring.util.response :refer [status]]
             [sanatoriocolegiales.lad-webhook.sql.ejecucion :refer [ejecuta!]]
-            [sanatoriocolegiales.lad-webhook.sql.enunciados :refer [selecciona-guardia]]))
-
-(defn extraer-fecha-y-hora
-  [date-str]
-  )
+            [sanatoriocolegiales.lad-webhook.sql.enunciados :refer [selecciona-guardia]]
+            [sanatoriocolegiales.lad-webhook.historiasclinicas.utils :refer [extraer-fecha-y-hora]]))
 
 (defn valida-request
-  [req]
-  (if req
-    req
-    (status "Solicitud no autorizada" 401)))
+  [req-body]
+  (if req-body
+    req-body
+    (throw (ex-info "Solicitud no autorizada" req-body)))) 
 
 (defn valida-paciente
-  [conn req]
-  (let [{{:keys [datetime event_object]} :body} req
+  "Si tiene éxito añade al cuerpo el registro del paciente, la fecha, la hora y lo devuelve.
+   Arroja excepción si la validación no es exitosa"
+  [conn req-body]
+  (let [{:keys [datetime event_object]} req-body
         {{:keys [uid]} :patient_external_id} event_object
         [fecha hora] (extraer-fecha-y-hora datetime)
         paciente (ejecuta! conn (selecciona-guardia uid fecha hora))]
     (if (> (count paciente) 0)
-      (assoc req :paciente paciente)
+      (assoc req-body :paciente paciente :fecha fecha :hora hora)
       (throw (ex-info "Paciente no encontrado" {:fecha fecha
                                                 :hora hora
                                                 :hc uid})))))
