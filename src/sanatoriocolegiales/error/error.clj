c51a29568f7ea34610930b75d0d30fc42f409aba
José Javier Blanco Rivero <javierweiss@gmail.com>
2024-08-01 13:20:53 -0300

    Crear un handler de errores

--- src/sanatoriocolegiales/error/error.clj
+++ src/sanatoriocolegiales/error/error.clj
@@ -1,6 +1,16 @@
-(ns sanatoriocolegiales.error.error)
+(ns sanatoriocolegiales.error.error
+  (:require [ring.util.response :refer [status]]))
 
 (defn lanza-error
   [^Throwable err]
   (assoc (ex-data err)
          :error (ex-message err)))
+
+(defn respuesta-http-de-error
+  [^Throwable err]
+  (let [mensaje (ex-message err)]
+    (condp = mensaje
+      "Solicitud no autorizada" (status "Solicitud no autorizada" 401)
+      "Paciente no encontrado" (status "El paciente referido no fue encontrado" 404)
+      (status (str "Hubo un error inesperado en el servidor:\n" mensaje) 500))))
+
