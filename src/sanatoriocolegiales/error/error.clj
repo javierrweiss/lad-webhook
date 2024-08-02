8504a9e564d8327e6acac243076f1443607a6dd6
José Javier Blanco Rivero <javierweiss@gmail.com>
2024-08-02 15:07:42 -0300

    minutiae

--- src/sanatoriocolegiales/error/error.clj
+++ src/sanatoriocolegiales/error/error.clj
@@ -14,7 +14,7 @@
    (merge 
     exception/default-handlers
     {SQLException (partial handler (status {:body "Hubo un error con la base de datos"} 500)) 
-     ::no-autorizada (partial handler (status {:body "Solicitud no autorizada (desde middleware)"} 401))
+     ::no-autorizada (partial handler (status {:body "Solicitud no autorizada"} 401))
      ::recurso-no-encontrado (partial handler (status {:body "No se encontró el paciente"} 404))
      ::exception/wrap (fn [handler e request]
                         (mulog/log ::excepcion-en-solicitud :mensaje (ex-message e) :fecha (LocalDateTime/now) :solicitud request)
