82d0c8ab535b391598899b40bb3c8fa7bf383467
José Javier Blanco Rivero <javierweiss@gmail.com>
2024-08-09 16:46:22 -0300

    Adaptar funciones de procesamiento a nueva estructura e ir completando datos para crear registros

--- src/sanatoriocolegiales/lad_webhook/historiasclinicas/lad_guardia.clj
+++ src/sanatoriocolegiales/lad_webhook/historiasclinicas/lad_guardia.clj
@@ -1,49 +1,38 @@
 (ns sanatoriocolegiales.lad-webhook.historiasclinicas.lad-guardia
-  (:require [sanatoriocolegiales.lad-webhook.sql.enunciados :refer [inserta-en-tbc-histpac inserta-en-tbl-hist-txt actualiza-tbc-guardia]]
-            [sanatoriocolegiales.lad-webhook.sql.ejecucion :refer [ejecuta!]]
+  (:require [sanatoriocolegiales.lad-webhook.sql.enunciados :refer [inserta-en-tbc-histpac 
+                                                                    inserta-en-tbc-histpac-txt 
+                                                                    actualiza-tbc-guardia]]
+            [sanatoriocolegiales.lad-webhook.sql.ejecucion :refer [ejecuta! obtiene-numerador!]]
             [hyperfiddle.rcf :refer [tests]]))
 
-(defn request->registros
+(defn prepara-registros
   "Adapta el mapa que viene del request y devuelve un vector con tres registros (también vectores) listos para ser persistidos"
-  [request-body]
-  (let [main-map (merge 
-                  (select-keys (:event_object request-body) [:call_diagnosis 
-                                                             :call_cie10 
-                                                             :call_motive
-                                                             :call_doctor_comment
-                                                             :patient_name
-                                                             :patient_external_id]) 
-                  (select-keys request-body [:paciente
-                                             :fecha
-                                             :hora]))
-        hc (get-in main-map [:patient_external_id :uid])]
-    [nil nil nil]))
+  [{:keys [guar-hist-clinica guar-fecha-ingreso guar-hora-ingreso hora_final_atencion fecha_inicio_atencion guar-obra guar-plan diagnostico motivo historia nombre patologia histpactratam]}] 
+  [[guar-hist-clinica guar-fecha-ingreso guar-hora-ingreso diagnostico hora_final_atencion fecha_inicio_atencion]
+   [guar-hist-clinica guar-fecha-ingreso]
+   [guar-hist-clinica guar-fecha-ingreso guar-hora-ingreso historia motivo "" ""]])
 
 (defn crea-historia-clinica
   "Persiste 3 registros a sus respectivas tablas. Recibe una conexión y tres vectores con los datos a ser persistidos"
   [db registro-guardia registro-historia-paciente registro-historia-texto]
-  (let [maestros (-> db :maestros)
-        desal (-> db :desal)
+  (let [#_#_maestros (-> db :maestros)
+        #_#_desal (-> db :desal)
         asistencial (-> db :asistencial)]
-    (ejecuta! asistencial (inserta-en-tbc-histpac registro-historia-paciente))
-    (ejecuta! desal (inserta-en-tbl-hist-txt registro-historia-texto))
-    (ejecuta! asistencial (actualiza-tbc-guardia registro-guardia)))) ;; Estas grabaciones se podrían hacer en paralelo
+    (future (ejecuta! asistencial (inserta-en-tbc-histpac registro-historia-paciente)))
+    (future (ejecuta! asistencial (inserta-en-tbc-histpac-txt registro-historia-texto)))
+    (future (ejecuta! asistencial (actualiza-tbc-guardia registro-guardia)))))
 
 (defn persiste-historia-clinica
-  "Toma el request y crea la historia clínica del paciente. Recibe el request y la conexión a la BD."
-  [db request-body]
-  (let [[guardia hc hc-texto] (request->registros request-body)]
+  "Toma la información del paciente y crea la historia clínica. Recibe el request y la conexión a la BD."
+  [db paciente]
+  (let [histpactratam (obtiene-numerador! db)
+        [guardia hc hc-texto] (prepara-registros (assoc paciente :histpactratam histpactratam))]
     (crea-historia-clinica db guardia hc hc-texto)))
 
 (comment
-  
+
   (let [m {:a 1
            :z {:b 12 :c 332}}]
-    (merge (select-keys (:z m) [:c]) (select-keys m [:a]))) 
-  
-  )
+    (merge (select-keys (:z m) [:c]) (select-keys m [:a]))))
 
-(tests
- 
- 
- )
+(tests)
