015868ca97333f1876d23f3c476d011a46e8b2f7
José Javier Blanco Rivero <javierweiss@gmail.com>
2025-04-17 13:38:40 -0300

    Devolver json en las respuestas de error

--- src/sanatoriocolegiales/lad_webhook/error/error.clj
+++ src/sanatoriocolegiales/lad_webhook/error/error.clj
@@ -1,7 +1,8 @@
 (ns sanatoriocolegiales.lad-webhook.error.error
   (:require [ring.util.response :refer [status]]
             [reitit.ring.middleware.exception :as exception]
-            [com.brunobonacci.mulog :as mulog])
+            [com.brunobonacci.mulog :as mulog]
+            [cheshire.core :as json])
   (:import java.sql.SQLException
            java.time.LocalDateTime))
 
@@ -13,11 +14,26 @@
   (exception/create-exception-middleware 
    (merge 
     exception/default-handlers
-    {SQLException (partial handler (status {:body "Hubo un error con la base de datos"} 500))
-     ::excepcion-sql (partial handler (status {:body "Hubo un error con la base de datos"} 500))
-     ::no-autorizada (partial handler (status {:body "Solicitud no autorizada"} 401))
-     ::recurso-no-encontrado (partial handler (status {:body "No se encontró el paciente"} 404))
-     ::bad-request (partial handler (status {:body "El objeto event-object no tiene la forma esperada"} 400))
+    {SQLException (partial handler (status
+                                    {:headers {"Content-Type" "application/json"}
+                                     :body (json/encode {:mensaje "Hubo un error con la base de datos"})}
+                                    500))
+     ::excepcion-sql (partial handler (status
+                                       {:headers {"Content-Type" "application/json"}
+                                        :body (json/encode {:mensaje "Hubo un error con la base de datos"})} 
+                                       500))
+     ::no-autorizada (partial handler (status 
+                                       {:headers {"Content-Type" "application/json"}
+                                        :body (json/encode {:mensaje "Solicitud no autorizada"})} 
+                                       401))
+     ::recurso-no-encontrado (partial handler (status 
+                                               {:headers {"Content-Type" "application/json"}
+                                                :body (json/encode {:mensaje "No se encontró el paciente"})} 
+                                               404))
+     ::bad-request (partial handler (status
+                                     {:headers {"Content-Type" "application/json"}
+                                      :body (json/encode {:mensaje "El objeto event-object no tiene la forma esperada"})}
+                                     400))
      ::exception/wrap (fn [handler e request]
                         (mulog/log ::excepcion-en-solicitud :mensaje (ex-message e) :fecha (LocalDateTime/now) :solicitud request)
                         (handler e request))}))) 
